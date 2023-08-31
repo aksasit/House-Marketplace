6925044c26268670be26f3e1e361f8bd7295e923
asit <sahooasit621@gmail.com>
2023-09-01 01:03:04 +0530

    Firebase Authentication and Profile

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,94 @@
+import { getAuth, updateProfile } from "firebase/auth"
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { updateDoc, doc } from "firebase/firestore"
+import { db } from "../firebase.config"
+import { toast } from "react-toastify"
+
+function Profile() {
+  const auth = getAuth()
+  const [changeDetails, setChangeDetails] = useState(false)
+  const [formData, setFormData] = useState({
+     name: auth.currentUser.displayName,
+     email: auth.currentUser.email,
+  }) 
+
+  const { name, email } = formData
+
+  const navigate = useNavigate()
+
+  const onLogout = () => {
+    auth.signOut()
+    navigate('/')
+  }
+
+  const onSubmit = async () => {
+    try {
+      if(auth.currentUser.displayName !== name){
+        await updateProfile(auth.currentUser, {
+          displayName: name
+        })
+
+        const userRef = doc(db, 'users', auth.currentUser.uid)
+        await updateDoc(userRef, {
+          name: name,
+        })
+      }
+    } catch (error){
+        toast.error('Could not update profile details')
+    }
+  }
+
+  const onChange = (e) => {
+    setChangeDetails((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value
+    }))
+  }  
+
+  return (
+    <div className="profile">
+        <header className="profileHeader">
+            <p className="pageHeader">My Profile</p>
+            <button type='button' className="logout" onClick={onLogout}>
+                Logout
+            </button>
+        </header>
+        
+        <main>
+          <form onSubmit={onSubmit}>
+            <div className="profileDetailsHeader">
+                <p className="profileDetailsText">Personal Details</p>
+                <p className="changePersonalDetails" onClick={() => {
+                    changeDetails && onsubmit()
+                    setChangeDetails((prevState) => !prevState)
+                }}>
+                    {changeDetails ? 'Done' : 'Change'}
+                </p>
+            </div>
+        
+            <div className="profileCard">
+                <input 
+                  type="text"
+                  id="name" 
+                  className={changeDetails ? 'profileNameActive' : 'profileName'}
+                  disabled={!changeDetails}
+                  value={name}
+                  onChange={onChange}  
+                />
+                <input 
+                  type="text"
+                  id="email" 
+                  className={changeDetails ? 'profileEmailActive' : 'profileEmail'}
+                  disabled={!changeDetails}
+                  value={email}
+                  onChange={onChange}  
+                />
+            </div>
+          </form>  
+        </main>
+    </div>
+  )
+}
+
+export default Profile
