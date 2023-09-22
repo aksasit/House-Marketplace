634bc6afaedd958fa806a8929d341a8476141068
asit <sahooasit621@gmail.com>
2023-09-23 02:54:00 +0530

    Updated package, imageUrls, Slider version

--- src/App.js
+++ src/App.js
@@ -31,7 +31,10 @@ function App() {
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/create-listing' element={<CreateListing />} />
           <Route path='/edit-listing/:listingId' element={<EditListing />} />
-          <Route path='/category/:categoryName/:listingId' element={<Listing />} />
+          <Route
+            path='/category/:categoryName/:listingId'
+            element={<Listing />}
+          />
           <Route path='/contact/:landlordId' element={<Contact />} />
         </Routes>
         <Navbar />
--- src/pages/EditListing.jsx
+++ src/pages/EditListing.jsx
@@ -14,8 +14,7 @@ import { v4 as uuidv4 } from 'uuid'
 import Spinner from '../components/Spinner'
 
 function EditListing() {
-  // eslint-disable-next-line
-  const [geolocationEnabled, setGeolocationEnabled] = useState(true)
+  // eslint-disable-next-line  
   const [loading, setLoading] = useState(false)
   const [listing, setListing] = useState(false)
   const [formData, setFormData] = useState({
@@ -117,33 +116,7 @@ function EditListing() {
       return
     }
 
-    let geolocation = {}
-    let location
-
-    if (geolocationEnabled) {
-      const response = await fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
-      )
-
-      const data = await response.json()
-
-      geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
-      geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
-
-      location =
-        data.status === 'ZERO_RESULTS'
-          ? undefined
-          : data.results[0]?.formatted_address
-
-      if (location === undefined || location.includes('undefined')) {
-        setLoading(false)
-        toast.error('Please enter a correct address')
-        return
-      }
-    } else {
-      geolocation.lat = latitude
-      geolocation.lng = longitude
-    }
+    let geolocation = { lat: latitude, lng: longitude } 
 
     // Store image in firebase
     const storeImage = async (image) => {
@@ -186,7 +159,7 @@ function EditListing() {
       })
     }
 
-    const imgUrls = await Promise.all(
+    const imageUrls = await Promise.all(
       [...images].map((image) => storeImage(image))
     ).catch(() => {
       setLoading(false)
@@ -196,7 +169,7 @@ function EditListing() {
 
     const formDataCopy = {
       ...formData,
-      imgUrls,
+      imageUrls,
       geolocation,
       timestamp: serverTimestamp(),
     }
@@ -378,7 +351,7 @@ function EditListing() {
             required
           />
 
-          {!geolocationEnabled && (
+          {(
             <div className='formLatLng flex'>
               <div>
                 <label className='formLabel'>Latitude</label>
